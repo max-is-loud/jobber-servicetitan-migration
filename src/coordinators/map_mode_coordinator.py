0f340bd9337f7e090a4f92a6f881a72d267656cc
Maxime Langlois-Morin <max@maxmorin.ca>
2025-11-19 22:13:27 -0800

    chore: clear map progress between entity tasks

--- src/coordinators/map_mode_coordinator.py
+++ src/coordinators/map_mode_coordinator.py
@@ -129,6 +129,9 @@ class MapModeCoordinator:
             transient=True,
         ) as progress:
             for entity_type in entity_types:
+                # Clear prior task rendering before starting next task to avoid clutter
+                progress.refresh(clear=True)
+
                 # Create progress task
                 task_id = progress.add_task(f"Mapping {entity_type}...", total=None, mapped=0)
 
@@ -151,6 +154,7 @@ class MapModeCoordinator:
                 )
                 try:
                     progress.remove_task(task_id)
+                    progress.refresh(clear=True)
                 except KeyError:
                     pass
 
